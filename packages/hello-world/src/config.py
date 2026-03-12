5e53e86a37cc0885f1aabe101777a23972a28f14
Garrett Gruss <garrett.g.gruss@gmail.com>
2026-03-12 14:36:19 -0500

    fix(hello-world): fix env names

--- packages/hello-world/src/config.py
+++ packages/hello-world/src/config.py
@@ -6,8 +6,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
-OPENAI_ORGANIZATION = os.environ["OpenAI-Organization"]
-OPENAI_PROJECT = os.environ["OpenAI-Project"]
+OPENAI_ORGANIZATION = os.environ["OPENAI_ORGANIZATION"]
+OPENAI_PROJECT = os.environ["OPENAI_PROJECT"]
 
 OPENAI_MODEL = "gpt-5-mini"
 TEMPORAL_SERVER = "localhost:7233"
